db77f5bb1b19c85b2bcc92af47a0db1683e9a211
Pouya <pouya.mozafar@gmail.com>
2019-08-02 00:40:52 +0430

    feat: add fluid section

---
+++ src/components/Home/FluidSection/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Section from '../../shared-components/Section';
+import SectionInfo from '../../shared-components/SectionInfo';
+import Button from '../../shared-components/Button';
+
+import './style.scss';
+
+import { sections } from '../../../locale/en';
+
+const { fluid: { info, buttons: { clickMe, hoverMe, selectMe } } } = sections;
+
+export default function FluidSection(){
+  return (
+    <Section>
+      <div className="fluid-section">
+        <SectionInfo
+          {...info}
+        />
+        <div className="fluid-section__actions">
+          <Button
+            text={clickMe}
+          />
+          <Button
+            text={hoverMe}
+          />
+          <Button
+            text={selectMe}
+          />
+        </div>
+      </div>
+    </Section>
+  );
+}
