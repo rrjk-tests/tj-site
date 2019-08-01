f678240a746d0ff256165bbccb987ae5129c9f7a
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2019-08-01 19:00:28 +0430

    feat: Helmet configured

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import Landing from './Landing';
+import Landing from './Landing'
+import { Helmet } from 'react-helmet'
 
 export default () => {
     return (
         <React.Fragment>
+            <Helmet>
+                <title>TheatreJS - Web-based animation tool</title>
+                <meta name="description" content="web-based animation tool" />
+                <meta name="keywords" content="animations,js,theatre" />
+            </Helmet>
             <Landing />
         </React.Fragment>
     )
