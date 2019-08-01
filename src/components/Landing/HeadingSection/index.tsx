2e76ebf9afad0a57b9aa415edb17a6ac64606c57
Pouya <pouya.mozafar@gmail.com>
2019-08-01 17:11:14 +0430

    feat: HeadingSection added

--- src/components/Landing/HeadingSection/index.tsx
+++ src/components/Landing/HeadingSection/index.tsx
@@ -2,13 +2,28 @@ import React from 'react';
 
 import Section from '../../shared-components/Section';
 
+import logo from './logo.svg';
 import './style.scss';
 
 export default function HeadingSection() {
   return (
     <Section>
-      <div>
-
+      <div className="heading-section">
+        <img
+          className="heading-section__logo"
+          src={logo}
+        />
+        <div className="heading-section__content">
+          <h2 className="heading-section__title">
+            <span>Artistry</span>
+            <span>
+              done easily
+            </span>
+          </h2>
+          <p className="heading-section__desc">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
       </div>
     </Section>
   );
