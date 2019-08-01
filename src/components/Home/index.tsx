a997facde70f3eae42d45dedc83ce313444501f8
Pouya <pouya.mozafar@gmail.com>
2019-08-02 00:41:02 +0430

    chore: add fluid-section to the Home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 import './style.scss';
 
 import HeadingSection from './HeadingSection';
+import FluidSection from './FluidSection';
 
 export default function App(){
     return (
         <div className="app">
             <HeadingSection />
+            <FluidSection />
         </div>
     );
 }
