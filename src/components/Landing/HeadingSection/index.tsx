3e2c36904c62485ccb0bf2f9b680efbe41630c23
Pouya <pouya.mozafar@gmail.com>
2019-08-01 16:37:30 +0430

    feat: basic HeadingSection added

---
+++ src/components/Landing/HeadingSection/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Section from '../../shared-components/Section';
+
+import './style.scss';
+
+export default function HeadingSection() {
+  return (
+    <Section>
+      <div>
+
+      </div>
+    </Section>
+  );
+}
