fd6cd463be11e23a7c87d070ed281816004da74e
Pouya <pouya.mozafar@gmail.com>
2019-08-02 01:37:34 +0430

    feat: Footer added

---
+++ src/components/Home/Footer/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './style.scss';
+
+import FooterColumn from './FooterColumn';
+
+import { footerSections } from '../../../locale/en';
+
+export default function Footer(){
+  const footerColumns = footerSections.map(item => (<FooterColumn title={item.title} description={item.description} />));
+
+  return (
+    <footer className="footer">
+      <div className="footer__layout">
+        { footerColumns }
+      </div>
+    </footer>
+  );
+}
