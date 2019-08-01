f92d19b164faf6e13c083a1db8e060a849e42e3b
Pouya <pouya.mozafar@gmail.com>
2019-08-01 16:37:12 +0430

    feat: Landing index.tsx added

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-import './style.scss'
+import './style.scss';
+
+import HeadingSection from './HeadingSection';
 
 export default function App(){
     return (
         <div className="app">
-            
+            <HeadingSection />
         </div>
     );
 }
