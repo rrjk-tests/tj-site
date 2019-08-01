65adb549d8e8985067d1f061add31b5ed08d0430
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2019-08-01 22:39:46 +0430

    fix: hompage added to package.json + docs re-build

---
+++ docs/precache-manifest.69105c2222ea419a1c8351806bbf9382.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9081bf46240d089ceb7e838faf54e483",
+    "url": "./index.html"
+  },
+  {
+    "revision": "44a0b11bc86e5f1127c9",
+    "url": "./static/css/main.48c41899.chunk.css"
+  },
+  {
+    "revision": "d3eff281891944c2a95b",
+    "url": "./static/js/2.31e0fa3a.chunk.js"
+  },
+  {
+    "revision": "44a0b11bc86e5f1127c9",
+    "url": "./static/js/main.8da94b09.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "20372e4d9029a3dd308e192fc3d45ef3",
+    "url": "./static/media/glow.20372e4d.svg"
+  },
+  {
+    "revision": "f1f56529dda0bc7f8328b44de551e847",
+    "url": "./static/media/line.f1f56529.svg"
+  },
+  {
+    "revision": "4842777e2936fc4d4461825fc7e1a55f",
+    "url": "./static/media/logo.4842777e.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5bd794b108cd96edd849b3d344af006f.js"
+  "./precache-manifest.69105c2222ea419a1c8351806bbf9382.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
