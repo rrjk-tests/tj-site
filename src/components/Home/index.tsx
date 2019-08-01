79bd810ed852be8925f9423a1e360f57783b0462
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2019-08-02 01:17:26 +0430

    feat: ControlSection was made

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 import './style.scss';
 
 import HeadingSection from './HeadingSection';
+import ControlSection from './ControlSection';
 
 export default function App(){
     return (
         <div className="app">
             <HeadingSection />
+            <ControlSection />
         </div>
     );
 }
