cddfdd2c38e6e7791cf08c2d0b1e35d4eb4b9b21
Pouya <pouya.mozafar@gmail.com>
2019-08-01 16:13:33 +0430

    chore: restructure

---
+++ src/components/Landing/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './style.scss'
+
+export default function App(){
+    return (
+        <div className="app">
+            
+        </div>
+    );
+}
---
+++ src/components/shared-components/Section/index.tsx
@@ -0,0 +1,21 @@
+import React, { ReactChildren } from 'react';
+
+import './style.scss';
+
+interface IProps {
+  line: string | null,
+  children: ReactChildren
+}
+
+export default function Section({ line = null, children }: IProps) {
+  return (
+    <section className="section">
+      <div className="section__line">
+        { line }
+      </div>
+      <div className="section__layout">
+        { children }
+      </div>
+    </section>
+  );
+}
