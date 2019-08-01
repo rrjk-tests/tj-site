316ca9e901c8028e54e3f6ee7d2a5457ad7d1832
Pouya <pouya.mozafar@gmail.com>
2019-08-02 01:22:14 +0430

    chore: add GetAhead to the Home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -4,12 +4,14 @@ import './style.scss';
 
 import HeadingSection from './HeadingSection';
 import FluidSection from './FluidSection';
+import GetAhead from './GetAhead';
 
 export default function App(){
     return (
         <div className="app">
             <HeadingSection />
             <FluidSection />
+            <GetAhead />
         </div>
     );
 }
