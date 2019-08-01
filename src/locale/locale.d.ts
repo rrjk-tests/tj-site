b7314e0c9dbc417081bc9bf3d295c385e5fbc710
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2019-08-01 17:48:37 +0430

    feat: glow.svg added

--- src/locale/locale.d.ts
+++ src/locale/locale.d.ts
@@ -2,8 +2,12 @@ interface ISection {
     title: string[],
     description: string[]
 }
-
 type TSections = {[key: string]: ISection}
+
+interface IFooterSection {
+    title: string,
+    description: string
+}
 
 interface ILocale {
     sections: TSections
