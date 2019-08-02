df3c8517246f58fa49619f414906886d2cfaeb9d
Pouya <pouya.mozafar@gmail.com>
2019-08-02 19:52:02 +0430

    feat: Loading added

---
+++ src/components/shared-components/Loading/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './style.scss';
+
+export default function Loading(){
+  return (
+    <div className="loading">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+  );
+};
