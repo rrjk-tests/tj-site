3f67ee7e9f2169d744a437897b9ab9df1600b6f5
Pouya <pouya.mozafar@gmail.com>
2019-08-02 19:51:53 +0430

    chore: Button can be disabled or loading now

--- src/components/shared-components/Button/index.tsx
+++ src/components/shared-components/Button/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './style.scss';
+import Loading from '../Loading';
 
 interface IProps {
   styleType?: 'primary' | 'secondary' | 'tertiary',
@@ -10,22 +11,27 @@ interface IProps {
   type?: "button" | "submit",
   className?: string,
   color?: string,
+  loading?: boolean,
+  isDisabled?: boolean,
 }
 
-export default function Button({ styleType = 'primary', hasOutline = false, text = '', type = 'button', className, color, onClick = ()=>{} }: IProps){
+export default function Button({ styleType = 'primary', isDisabled = false, hasOutline = false, text = '', type = 'button', className, color, loading = false, onClick = ()=>{} }: IProps){
   let customStyle = {};
   if(hasOutline && color) customStyle = { borderColor: color };
   if(!hasOutline && color) customStyle = { backgroundColor: color };
   const outlineModifier = hasOutline ? ' button--outline' : '';
   const styleModifier = ` button--${styleType}`;
+  const loadingModifier = loading ? ' button--loading' : '';
+  const isDisabledModifier = isDisabled ? ' button--disabled' : '';
   return (
     <button
       style={customStyle}
-      className={`button${outlineModifier}${styleModifier} ${className ? className : ''}`}
+      className={`button${outlineModifier}${styleModifier}${loadingModifier}${isDisabledModifier} ${className ? className : ''}`}
       onClick={onClick}
       type={type}
+      disabled={isDisabled}
     >
-      { text }
+      { loading ? <Loading /> : text }
     </button>
   );
 }
