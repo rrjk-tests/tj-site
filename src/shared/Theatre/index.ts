23303039ad00fdae385b02429c7eff9114f66844
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2019-08-06 20:48:12 +0430

    shared/Theatre: state was added

--- src/shared/Theatre/index.ts
+++ src/shared/Theatre/index.ts
@@ -1,6 +1,7 @@
 import { getProject } from 'theatre'
+import state from './state.json'
 
-export const project = getProject('Theatre Website')
+export const project = getProject('Theatre Website', { state })
 
 export const TLControlBox = project.getTimeline('Control Box')
 export const TLEasyAnimation = project.getTimeline('Easy Animation / Bouncing Ball')
