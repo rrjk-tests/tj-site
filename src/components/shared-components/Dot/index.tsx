2a77ebfcefd5e37eac273507222b44042eb43b67
Pouya <pouya.mozafar@gmail.com>
2019-08-07 00:20:07 +0430

    chore: Dot accepts other positions as well

--- src/components/shared-components/Dot/index.tsx
+++ src/components/shared-components/Dot/index.tsx
@@ -4,13 +4,15 @@ import './style.scss';
 
 interface IProps {
   type?: 'primary' | 'secondary' | 'tertiary' | 'white' | string,
-  left?: string,
-  top?: string,
+  left?: number | string,
+  top?: number | string,
+  bottom?: number | string,
+  right?: number | string,
 }
 
-export default function Dot({ type = 'primary', left = "0px", top = "0px" }: IProps){
+export default function Dot({ type = 'primary', left = 'auto', top = 'auto', bottom = 'auto', right = 'auto' }: IProps){
   const modifier = ` dot--${type}`;
-  const customStyles = { left, top };
+  const customStyles = { left, top, bottom, right };
   return (
     <div
       style={customStyles}
