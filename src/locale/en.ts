987faa24bf05bd5419321d031725e3442b9b03ab
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2019-08-07 22:11:11 +0430

    src: some style changes

--- src/locale/en.ts
+++ src/locale/en.ts
@@ -15,7 +15,7 @@ export const sections: TSections = {
     fluid: {
         info: {
             title: ['Fluid animation'],
-            description: ['A vision statement is a company\'s road map, indicating', 'what the company wants to become by setting a defined', 'direction for the company\'s growth.']
+            description: ['A vision statement is a company\'s road map, indicating what the company wants to become by setting a defined direction for the company\'s growth.']
         },
         buttons: {
             clickMe: 'Click me',
@@ -33,7 +33,7 @@ export const sections: TSections = {
     getAhead: {
         info: {
             title: ['Get ahead'],
-            description: ['Short text with copy saying something like:', '- short time to get ahead of the others and try something new', '- 1on1 contact with the developers ? (exclusivity marketing)']
+            description: ['Short text with copy saying something like: short time to get ahead of the others and try something new 1on1 contact with the developers ? (exclusivity marketing)']
         },
         subscribe: {
             placeholder: 'master_artist@gmail.com',
