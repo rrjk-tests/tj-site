e6997f6ad8f2edf35f176e3f1c2ca94a0abf8076
Pouya <pouya.mozafar@gmail.com>
2019-08-07 23:04:04 +0430

    chore: dots removed

--- src/components/Home/EasyAnimation/index.tsx
+++ src/components/Home/EasyAnimation/index.tsx
@@ -11,14 +11,17 @@ import lineSVG from './line.svg';
 import mobileLineSVG from './mobileLine.svg';
 import Dot from '../../shared-components/Dot';
 import BouncingBall from './BouncingBall';
+import { useMobile } from '../../../shared/useMobile';
 
 const { info } = sections.easyAnimation;
 
 export default function EasyAnimationSection() {
+  const { isMobile } = useMobile();
+
   return (
     <Section line={lineSVG} mobileLine={mobileLineSVG}>
       <div className="easy-animation-section">
-        <Dot bottom={0} left="45%" type="primary" />
+        { !isMobile ? <Dot bottom={0} left="45%" type="primary" /> : null }
         <Dot top={0} type="tertiary" left={20} />
         <SectionInfo
           direction="rtl"
