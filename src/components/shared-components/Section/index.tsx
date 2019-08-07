a89dd083d304fb595e20f680d4815cdc93a3d9b9
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2019-08-07 23:01:35 +0430

    src: scroll bouncing ball

--- src/components/shared-components/Section/index.tsx
+++ src/components/shared-components/Section/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 import './style.scss';
+import { useMobile } from '../../../shared/useMobile';
 // import Line from './Line';
 
 interface IProps {
@@ -13,16 +14,10 @@ interface IProps {
 export default function Section({ line, mobileLine, children, hasGlown = false }: IProps) {
   const hasGlownModifier = hasGlown ? ' section--glown' : ''
   const hasLineModifier = !line && !mobileLine ? ' section--without-line' : '';
-  const [innerWidth, setInnerWidth] = useState(window.innerWidth)
+  
+  const { isMobile } = useMobile()
 
-  const modifyWindowWidth = () => setInnerWidth(window.innerWidth)
-
-  useEffect(()=>{
-    window.addEventListener('resize', modifyWindowWidth)
-    return () => window.removeEventListener('resize', modifyWindowWidth)
-  });
-
-  const style = (line && mobileLine) ? {backgroundImage: `url(${innerWidth > 800 ? line : mobileLine})`} : {};
+  const style = (line && mobileLine) ? {backgroundImage: `url(${isMobile ? mobileLine : line})`} : {};
   return (
     <section style={style} className={`section${hasGlownModifier}${hasLineModifier}`}>
       <div className="section__layout">
---
+++ src/shared/useMobile/index.ts
@@ -0,0 +1,17 @@
+import React, { useState, useEffect} from 'react';
+
+export const useMobile = () => {
+    const [innerWidth, setInnerWidth] = useState(window.innerWidth)
+
+    const modifyWindowWidth = () => setInnerWidth(window.innerWidth)
+
+    useEffect(()=>{
+        window.addEventListener('resize', modifyWindowWidth)
+        return () => window.removeEventListener('resize', modifyWindowWidth)
+    });
+
+    return {
+        innerWidth,
+        isMobile: innerWidth < 800
+    }
+}
