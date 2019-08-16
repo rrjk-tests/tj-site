21000ba225f41d6ffb1b381ccc9b1e1632535b81
AriaMinaei <aria.minaei@gmail.com>
2019-08-16 10:47:29 +0200

    Fix the error

--- src/components/Home/Footer/index.tsx
+++ src/components/Home/Footer/index.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 import "./style.scss"
-import logo from "./logo.svg"
+// import logo from "./logo.svg"
 
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer__layout">
-        <img src={logo} />
+        {/* <img src={logo} /> */}
       </div>
     </footer>
   )
